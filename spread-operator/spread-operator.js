2240593382cbd900250309e6ca968a0af295a188
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-09-23 19:20:01 +0600

    Updated

---
+++ spread-operator/spread-operator.js
@@ -0,0 +1,29 @@
+// Javascript Spread Operator
+
+
+// Creating Print Function
+let print = console.log.bind(document);
+
+
+
+//-> 1. Array Concatenation
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+const concat = [...array1, ...array2];
+
+print(concat);
+
+
+//-> 2. Array Copy
+const original = [1, 2, 3];
+const copy = [...original];
+
+print(copy);
+
+
+//-> 3. Object Copy
+const originalObject = {name: "John", age: 25};
+const copyObject = {...originalObject};
+
+print(copyObject);
